93087494cff393176ca9ec3ce5f9acff4eae387a
Twin Bukuru <twinbukuru@gmail.com>
2017-12-27 22:19:04 +0200

    settings host changed

--- frame/settings.py
+++ frame/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'impzy7cig4rppjm1mpqfynuyj&p_cm@!ut#gu$kbjm(o+zjb0b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
