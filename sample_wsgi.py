f0922325714ea76cd7cd5d2921dfc7e73751fc38
Twin Bukuru <twinbukuru@gmail.com>
2017-12-27 21:00:42 +0200

    Initial commit

---
+++ sample_wsgi.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+path= '/home/twinbukuru/frame'
+
+if path not in sys.path:
+	sys.path.append(path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "frame.settings")
+
+
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+
+
+application = StaticFilesHandler (get_wsgi_application())
+
